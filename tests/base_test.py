2b8ab82be740aeafcc04e7186cad7ebd64ce4356
mkezikov <mkezikov@sdvor.com>
2021-02-04 11:31:08 +0500

    добавил тест для метода delete

--- tests/base_test.py
+++ tests/base_test.py
@@ -8,21 +8,31 @@ class SuggestTestCase(unittest.TestCase):
         self.app = app
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
-        self.requests = [
+        self.get_requests = [
             {'phrase': 'rbhg', 'count': 10, 'status': 200},
             {'phrase': 'рейк нап', 'count': 8, 'status': 200},
             {'phrase': 'гипскор', 'count': '', 'status': 200},
             {'phrase': '', 'count': 2, 'status': 400},
             {'phrase': 'кирп шам огн', 'count': 0, 'status': 200},
         ]
+        self.del_requests = [
+            {'phrase': 'к', 'status': 200},
+            {'phrase': '', 'status': 400},
+        ]
 
     def test_get_response(self):
-        for request in self.requests:
+        for request in self.get_requests:
             phrase, count, status = request['phrase'], request['count'], request['status']
             response = self.client.get(f'/suggest?phrase={phrase}')
             self.assertEqual(response.status_code, status)
             if status == 200:
                 self.assertLessEqual(len(response.get_json()['response']), count or 10)
+
+    def test_del_response(self):
+        for request in self.del_requests:
+            phrase, status = request['phrase'], request['status']
+            response = self.client.delete(f'/suggest?phrase={phrase}')
+            self.assertEqual(response.status_code, status)
 
 
 if __name__ == "__main__":
